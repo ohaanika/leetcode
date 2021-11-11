c99a3570d74e6168bd1ac9e88a9d26ff5c203126
Aanika Rahman <aanikarahman@gmail.com>
2021-11-11 00:10:57 -0500

    feat(1188): update solution code

--- solutions/1188_design_bounded_blocking_queue.py
+++ solutions/1188_design_bounded_blocking_queue.py
@@ -4,9 +4,10 @@ https://leetcode.com/problems/design-bounded-blocking-queue/
 """
 
 from collections import deque
-from threading import Semaphore
+from threading import Semaphore, Lock
 
 
+# METHOD 1: 2 semaphores
 class BoundedBlockingQueue(object):
     def __init__(self, capacity: int):
         self.queue = deque()
@@ -21,6 +22,33 @@ class BoundedBlockingQueue(object):
     def dequeue(self) -> int:
         self.pop.acquire()
         element = self.queue.popleft()
+        self.push.release()
+        return element
+
+    def size(self) -> int:
+        return len(self.queue)
+
+
+# METHOD 2: 2 semaphores, 1 lock
+class BoundedBlockingQueue2(object):
+    def __init__(self, capacity: int):
+        self.queue = deque()
+        self.push = Semaphore(capacity)
+        self.pop = Semaphore(0)
+        self.lock = Lock()
+
+    def enqueue(self, element: int) -> None:
+        self.push.acquire()
+        self.lock.acquire()
+        self.queue.append(element)
+        self.lock.release()
+        self.pop.release()
+
+    def dequeue(self) -> int:
+        self.pop.acquire()
+        self.lock.acquire()
+        element = self.queue.popleft()
+        self.lock.release()
         self.push.release()
         return element
 
